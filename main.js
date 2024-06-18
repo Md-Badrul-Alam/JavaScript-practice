76a4383bf59dbc2f989eb6386ebafceb61ab54fd
badrul1977 <badaldp1977@gmail.copm>
2024-06-19 03:05:30 +0600

    less than and  greater than

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
-var myName = "Badal";
-var myAeg
+/* var myName = "Badal";
+var myAeg = 45
+console.log(myAeg); */
 
+/* var a = 10;
+var b = 20;
+var c = 30;
 
+var d = a + b + c;
+var f = 80;
+var e = f - d;
 
+console.log(a+b+c);
+ */
 
-console.log(typeof(myName));
+/* var a = 50;
+a++;
+console.log(a); */
+
+var a = 20;
+var b = 10;
+var c = 30;
+// A কি B এর চেয়ে ছোট
+var e = (a < b);
+console.log(e);
